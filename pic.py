5d2761f9dd4b03b03c9520a280c7b7a4eae49acf
Smirn0v-da <dan16533_2@mail.ru>
2020-10-11 20:18:48 +0300

    Исправлена функция рисования дома

--- pic.py
+++ pic.py
@@ -2,36 +2,66 @@ import pygame
 from pygame.draw import *
 import math
 import random as r
-#import numpy as np
 
 pygame.init()
 
 FPS = 30
 
 
-def house(x, y, k, n, m):
-    polygon(screen, (255, 165, 0), ((x, y), (x + k, y), (x + k, y - 2 * n),
-                                    (x - k, y - 2 * n), (x - k, y), (x, y)))
-    polygon(screen, (0, 0, 0), ((x, y), (x + k, y), (x + k, y - 2 * n),
-                                (x - k, y - 2 * n), (x - k, y), (x, y)), 1)
-    polygon(screen, (200, 0, 0), ((x, y - 2 * n), (x + (k + 10), y - 2 * n), (x, y - 3 * n),
-                                  (x - (k + 10), y - 2 * n), (x, y - 2 * n)))
-    polygon(screen, (0, 0, 0), ((x, y - 2 * n), (x + (k + 10), y - 2 * n), (x, y - 3 * n),
-                                (x - (k + 10), y - 2 * n), (x, y - 2 * n)), 1)
-    circle(screen, (173, 216, 230), (x, y - n), m)
-    circle(screen, (255, 255, 255), (x, y - n), m, 1)
-    line(screen, (255, 255, 255), (x, y - n + m), (x, y - n - m), 1)
-    line(screen, (255, 255, 255), (x + m, y - n), (x - m, y - n), 1)
+def draw_house(surface, x, y, width, height, window_radius):
+    '''
+    Функция рисует дом
+    surface - объект pygame.Surface
+    x, y - координаты нижней центральной точки дома
+    width - ширина стены дома
+    height - высота дома
+    window_radius - радиус окна
+    '''
 
+    def draw_wall(surface, x, y, width, height):
+        '''
+        Функция рисует стену дома
+        surface - объект pygame.Surface
+        x, y - координаты нижней центральной точки дома
+        width, height - ширина и высота стены дома
+        '''
+        polygon(surface, (255, 165, 0), [(x + width // 2, y), (x + width // 2, y - height),
+                                    (x - width // 2, y - height), (x - width // 2, y)])
+        polygon(surface, (0, 0, 0), [(x + width // 2, y), (x + width // 2, y - height),
+                                    (x - width // 2, y - height), (x - width // 2, y)], 1)
 
-#def sun(x, y, r, g, b):
-    #for i in range(37):
-        #c = i * np.pi / 36
-        #polygon(screen, (r, g, b), ((int(x + 50 * np.cos(c)), int(y - 50 * np.sin(c))),
-                                    #(int(x + 50 * np.cos(np.pi * 2 / 3 + c)),
-                                     #int(y - 50 * np.sin(np.pi * 2 / 3 + c))),
-                                    #(int(x + 50 * np.cos(np.pi * 4 / 3 + c)),
-                                     #int(y - 50 * np.sin(np.pi * 4 / 3 + c)))))
+    def draw_roof(surface, x, y, width, height):
+        '''
+        Функция рисует крышу дома
+        surface - объект pygame.Surface
+        x, y - координаты нижней центральной точки крыши
+        width, height - ширина и высота крыши
+        '''
+        polygon(surface, (200, 0, 0), [(x + width // 2, y), (x, y - height), (x - width // 2, y)])
+        polygon(surface, (0, 0, 0), [(x + width // 2, y), (x, y - height), (x - width // 2, y)], 1)
+
+    def draw_window(surface, x, y, r):
+        '''
+        Функция рисует окно
+        surface - объект pygame.Surface
+        x, y - координаты центра окна
+        r - радиус окна
+        '''
+        circle(surface, (173, 216, 230), (x, y), r)
+        circle(surface, (255, 255, 255), (x, y), r, 1)
+        line(surface, (255, 255, 255), (x, y + r), (x, y - r), 1)
+        line(surface, (255, 255, 255), (x - r, y), (x + r, y), 1)
+
+    wall_height = height * 2 // 3
+    roof_y = y - wall_height
+    roof_width = width + 20
+    roof_height = height - wall_height
+    window_y = y - height // 3
+
+    #рисование
+    draw_wall(surface, x, y, width, wall_height)
+    draw_roof(surface, x, roof_y, roof_width, roof_height)
+    draw_window(surface, x, window_y, window_radius)
 
 
 def cloud(x, y, r):
@@ -59,11 +89,11 @@ def tree(x, y, l, wid, r):
 screen = pygame.display.set_mode((800, 600))
 rect(screen, (135, 206, 250), (0, 0, 800, 350))
 rect(screen, (0, 255, 127), (0, 350, 800, 250))
-house(150, 430, 110, 75, 45)
+draw_house(screen, 150, 430, 220, 225, 45)
 tree(550, 270, 60, 15, 40)
 tree(700, 290, 65, 14, 39)
 tree(480, 300, 55, 13, 37)
-house(380, 520, 100, 60, 35)
+draw_house(screen, 380, 520, 200, 180, 35)
 cloud(200, 100, 35)
 cloud(580, 90, 40)
 cloud(50, 50, 25)
